2f33ef1bdfb51b705245c93d850f848675695fad
rezan21 <reza.naghshineh@gmail.com>
2019-08-28 19:31:54 +0100

    Updates

---
+++ precache-manifest.a462dbcca3c69bb1834587d78abdc7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a3e8d7340c2f3a8173f6327fe84c9e",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "62b5293ef1e5766bcc8c",
+    "url": "/recipe-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2ea841f865343560581f",
+    "url": "/recipe-app/static/js/2.3d9b810f.chunk.js"
+  },
+  {
+    "revision": "62b5293ef1e5766bcc8c",
+    "url": "/recipe-app/static/js/main.d2c1c657.chunk.js"
+  },
+  {
+    "revision": "2cdc37c46ee7a3de3f17",
+    "url": "/recipe-app/static/js/runtime~main.2c4ead4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
