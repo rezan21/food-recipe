ad0000045f5be53a456d0abc23b9e805c4218cc8
rezan21 <reza.naghshineh@gmail.com>
2019-08-31 00:29:45 +0100

    starter files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import React from 'react';
-const App = () => {
-  return <div>Hello world</div>;
-};
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
 export default App;
