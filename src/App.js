71b4502d69d051ba95618423ca3f01d684d24cd6
rezan21 <reza.naghshineh@gmail.com>
2019-09-01 18:04:23 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form';
+import Recipes from './components/Recipes';
 
+const API_KEY = '5f70cfa7a28864ac9b336a0e95042d8b';
 class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+    const call_api = await fetch(
+      `https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+    );
+    const data = await call_api.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
