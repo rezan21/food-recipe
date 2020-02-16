e8a697d1f759889d9e1e4dd321e2310aea615b25
Reza N <32329310+rezan21@users.noreply.github.com>
2020-02-16 15:34:23 +0000

    Update Recipe.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,9 @@
 import React from 'react';
+class Recipe extends React.Component {
+  render() {
+    console.log(this.props);
 
-const Recipe = () => {
-  return <div>hello</div>;
-};
+    return <div>sss</div>;
+  }
+}
 export default Recipe;
